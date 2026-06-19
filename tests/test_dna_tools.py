b5349379f46c3425d90930677a4138ba52a33f5c
jmunoz-R <jmmvazquez122@gmail.com>
2026-06-19 22:25:36 +0100

    Update test_dna_tools.py
    Add more tests

--- tests/test_dna_tools.py
+++ tests/test_dna_tools.py
@@ -3,3 +3,16 @@ from dna_tools import gc_percentage
 def test_gc_percentage():
     assert gc_percentage(["G","C","G","C"]) == 100
     assert gc_percentage(["A","T","A","T"]) == 0
+
+def test_gc():
+    assert gc_percentage(["G","C","G","C"]) == 100
+    assert gc_percentage(["A","T","A","T"]) == 0
+
+def test_complement():
+    assert get_complementary_strand(["A","T","C"]) == ["T","A","G"]
+
+def test_reverse_complement():
+    assert reverse_complement(["T","A","G"]) == ["G","A","T"]
+
+def test_translation():
+    assert translate(["A","T","G"]) == ["Met"]
