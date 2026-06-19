f80c56ab0b1a96630b13d089e8956e998f769257
jmunoz-R <jmmvazquez122@gmail.com>
2026-06-19 22:16:57 +0100

    Update genetic_code.py
    Correct errors and adding commentary

--- data/genetic_code.py
+++ data/genetic_code.py
@@ -1,7 +1,68 @@
 codon_table = {
+    # Phenylalanine
     "TTT": "Phe", "TTC": "Phe",
+
+    # Leucine
     "TTA": "Leu", "TTG": "Leu",
+    "CTT": "Leu", "CTC": "Leu", "CTA": "Leu", "CTG": "Leu",
+
+    # Isoleucine
+    "ATT": "Ile", "ATC": "Ile", "ATA": "Ile",
+
+    # Methionine (START)
     "ATG": "Met",
+
+    # Valine
+    "GTT": "Val", "GTC": "Val", "GTA": "Val", "GTG": "Val",
+
+    # Serine
+    "TCT": "Ser", "TCC": "Ser", "TCA": "Ser", "TCG": "Ser",
+    "AGT": "Ser", "AGC": "Ser",
+
+    # Proline
+    "CCT": "Pro", "CCC": "Pro", "CCA": "Pro", "CCG": "Pro",
+
+    # Threonine
+    "ACT": "Thr", "ACC": "Thr", "ACA": "Thr", "ACG": "Thr",
+
+    # Alanine
+    "GCT": "Ala", "GCC": "Ala", "GCA": "Ala", "GCG": "Ala",
+
+    # Tyrosine
+    "TAT": "Tyr", "TAC": "Tyr",
+
+    # STOP codons
     "TAA": "STOP", "TAG": "STOP", "TGA": "STOP",
-    # etc...
-}
+
+    # Histidine
+    "CAT": "His", "CAC": "His",
+
+    # Glutamine
+    "CAA": "Gln", "CAG": "Gln",
+
+    # Asparagine
+    "AAT": "Asn", "AAC": "Asn",
+
+    # Lysine
+    "AAA": "Lys", "AAG": "Lys",
+
+    # Aspartic Acid
+    "GAT": "Asp", "GAC": "Asp",
+
+    # Glutamic Acid
+    "GAA": "Glu", "GAG": "Glu",
+
+    # Cysteine
+    "TGT": "Cys", "TGC": "Cys",
+
+    # Tryptophan
+    "TGG": "Trp",
+
+    # Arginine
+    "CGT": "Arg", "CGC": "Arg", "CGA": "Arg", "CGG": "Arg",
+    "AGA": "Arg", "AGG": "Arg",
+
+    # Glycine
+    "GGT": "Gly", "GGC": "Gly", "GGA": "Gly", "GGG": "Gly"
+    }
+
