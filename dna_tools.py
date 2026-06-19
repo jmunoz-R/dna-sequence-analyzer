8e10cdee7bed4327564d1350b6a8c1219bf6d5d3
jmunoz-R <jmmvazquez122@gmail.com>
2026-06-19 22:13:01 +0100

    Update dna_tools.py
    Add the code

--- dna_tools.py
+++ dna_tools.py
@@ -1,1 +1,28 @@
+from data.genetic_code import codon_table
+from data.complement import com_bases
 
+def nucleotide_count(DNA_seq):
+    print("Nucleotide count: " + str(len(DNA_seq)))
+
+def gc_percentage(DNA_seq):
+    return ((DNA_seq.count("G") + DNA_seq.count("C")) / len(DNA_seq)) * 100
+
+def get_complementary_strand(DNA_seq):
+    return [com_bases[n] for n in DNA_seq]
+
+
+def reverse_complement(seq):
+    return seq[::-1]
+
+def translate(DNA_seq):
+    codons = []
+    for i in range(0, len(DNA_seq), 3):
+        codon = "".join(DNA_seq[i:i+3])
+        if len(codon) == 3:
+            codons.append(codon)
+
+    aa_seq = []
+    for codon in codons:
+        aa_seq.append(codon_table.get(codon, "X"))
+
+    return aa_seq
